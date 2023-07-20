42e9d67ad62c970dcf9597acce0f7671fbc031f3
Gom1031 <atheniald@gmail.com>
2023-07-20 14:52:16 +0900

    시작

---
+++ src/test/java/lawproject/lawproject/LawprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package lawproject.lawproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
