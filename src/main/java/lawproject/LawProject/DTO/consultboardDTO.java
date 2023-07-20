46101e5fe6e6d8ed5f32b1799f119d33d585f10c
Gom1031 <atheniald@gmail.com>
2023-07-20 17:35:05 +0900

    중간

---
+++ src/main/java/lawproject/LawProject/DTO/consultboardDTO.java
@@ -0,0 +1,12 @@
+package lawproject.LawProject.DTO;
+
+import lombok.Data;
+
+@Data
+public class consultboardDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private String writer;
+    private String date;
+}
